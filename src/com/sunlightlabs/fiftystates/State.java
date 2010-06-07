af061ceac4a70cee0d0e19b29c108850887a4338
Michael Stephens <mstephens@sunlightfoundation.com>
2010-06-07 11:13:09 -0400

    state metadata fields consistent with new API

--- src/com/sunlightlabs/fiftystates/State.java
+++ src/com/sunlightlabs/fiftystates/State.java
@@ -11,9 +11,10 @@ public class State extends FiftystatesObject {
 	public String abbreviation;
 	public String legislature_name;
 	public String upper_chamber_name, lower_chamber_name;
-	public String upper_title, lower_title;
-	public int upper_term, lower_term;
-	
+	public String upper_chamber_title, lower_chamber_title;
+	public int upper_chamber_term, lower_chamber_term;
+
+	private ArrayList<String> session_names = new ArrayList<String>();
 	public ArrayList<State.Session> sessions = new ArrayList<State.Session>();
 
 	public State(JSONObject json) throws JSONException {
@@ -24,18 +25,18 @@ public class State extends FiftystatesObject {
 		legislature_name = json.getString("legislature_name");
 		upper_chamber_name = json.getString("upper_chamber_name");
 		lower_chamber_name = json.getString("lower_chamber_name");
-		upper_title = json.getString("upper_chamber_title");
-		lower_title = json.getString("lower_chamber_title");
-		upper_term = json.getInt("upper_chamber_term");
-		lower_term = json.getInt("lower_chamber_term");
-			
+		upper_chamber_title = json.getString("upper_chamber_title");
+		lower_chamber_title = json.getString("lower_chamber_title");
+		upper_chamber_term = json.getInt("upper_chamber_term");
+		lower_chamber_term = json.getInt("lower_chamber_term");
+
 		JSONArray j_sessions = json.getJSONArray("sessions");
 		int length = j_sessions.length();
 		for (int i = 0; i < length; i++) {
-			sessions.add(new State.Session(j_sessions.getJSONObject(i)));
+			session_names.add(j_sessions.getString(i));
 		}
 	}
-	
+
 	public static State find(String abbreviation) throws FiftystatesException {
 		String url = Fiftystates.url(abbreviation, "");
 		try {
@@ -44,19 +45,27 @@ public class State extends FiftystatesObject {
 			throw new FiftystatesException(e, "Problem parsing the JSON from " + url);
 		}
 	}
-	
-	public Session getActiveSession() {
-		return sessions.get(0);
+
+	public String getActiveSessionName() {
+		return session_names.get(session_names.size() - 1);
 	}
 
 	public class Session {
 		public String name;
 		public int start_year, end_year;
-		
+
+		public ArrayList<String> sub_sessions = new ArrayList<String>();
+
 		public Session(JSONObject json) throws JSONException {
 			name = json.getString("name");
 			start_year = json.getInt("start_year");
 			end_year = json.getInt("end_year");
+
+			JSONArray j_subs = json.getJSONArray("sub_sessions");
+			int length = j_subs.length();
+			for (int i = 0; i < length; i++) {
+				sub_sessions.add(j_subs.getString(i));
+			}
 		}
 	}
 }
